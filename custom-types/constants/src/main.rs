6fed0bd31bec3c460ccb3b0f05f8044ac9356e04
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-05-18 03:12:42 +0545

    Constant example

--- custom-types/constants/src/main.rs
+++ custom-types/constants/src/main.rs
@@ -1,3 +1,21 @@
+use std::println;
+
+// Globals are declared outside all other scopes.
+static LANGUAGE: &str = "Rust";
+const THREASHOLD: i32 = 10;
+
+fn is_big(n: i32) -> bool {
+    // Access constant in some function
+    n > THREASHOLD
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 16;
+
+    // Access constant in the main thread
+    println!("This is {}", LANGUAGE);
+    println!("The threashold is {}", THREASHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else { "small" });
+
+    // THREASHOLD = 5; // ERR
 }
