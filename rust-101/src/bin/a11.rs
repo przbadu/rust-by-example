b79c1692c027fa1504f2d3fa84eda732952b925f
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-05-29 08:33:43 +0545

    Implementation exercise with impl

--- rust-101/src/bin/a11.rs
+++ rust-101/src/bin/a11.rs
@@ -9,4 +9,29 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
+
+// dont Transfer Ownership, of GroceryItem
+fn display_quantity(item: &GroceryItem) {
+    println!("Quantity: {:?}", item.quantity)
+}
+
+// dont Transfer Ownership, of GroceryItem
+fn display_id(item: &GroceryItem) {
+    println!("Id: {:?}", item.id)
+}
+
+fn main() {
+    let item = GroceryItem {
+        id: 1,
+        quantity: 10,
+    };
+
+    // Borrow item
+    display_quantity(&item);
+    // Borrow item
+    display_id(&item);
+} // item will be moved from memory here.
--- rust-101/src/bin/a12.rs
+++ rust-101/src/bin/a12.rs
@@ -10,4 +10,71 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("red"),
+            Color::Green => println!("green"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("depth: {:?}", self.depth);
+    }
+}
+
+struct ShippingBox {
+    dimensions: Dimensions,
+    weight: f32,
+    color: Color,
+}
+
+impl ShippingBox {
+    fn new(weight: f32, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color,
+        }
+    }
+
+    fn print(&self) {
+        println!("");
+        println!("Weight: {:?}", self.weight);
+        self.dimensions.print();
+        self.color.print();
+    }
+}
+
+fn main() {
+    let dimensions = Dimensions {
+        width: 24.0,
+        height: 20.0,
+        depth: 32.0,
+    };
+    let red_box = ShippingBox::new(10.0, Color::Red, dimensions);
+    red_box.print();
+
+    let dimensions = Dimensions {
+        width: 10.0,
+        height: 20.0,
+        depth: 15.0,
+    };
+    let green_box = ShippingBox::new(20.0, Color::Green, dimensions);
+    green_box.print();
+}
